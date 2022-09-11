d54665e7121ad7d01c96f08743577bafcb830175
Bruno-Araujo <101733555+Bruno-kitrim@users.noreply.github.com>
2022-09-10 23:15:43 -0300

    lógica para comparar letras e aparecer na tela

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let contadorChances = 6;
+let listaDinamica = [];
 let palavraSorteada;
 let dicaSorteada;
 
@@ -7,21 +9,82 @@ const listaPalavras = [
     dica: "FRUTA",
   }),
   (palavra002 = {
-    nome: "SÃO LUÍS",
+    nome: "SAO LUIS",
     dica: "LUGAR",
   }),
   (palavra002 = {
-    nome: "MIRINZAL",
-    dica: "LUGAR",
+    nome: "CANETA",
+    dica: "OBJETO",
   }),
 ];
 
 function geradorDePalavras() {
   const palavra = parseInt(Math.random() * listaPalavras.length);
-  console.log(palavra);
   palavraSorteada = listaPalavras[palavra].nome;
-  dicaSorteada = listaPalavras[palavra].dica;
   console.log(palavraSorteada);
-  console.log(dicaSorteada);
+  dicaSorteada = listaPalavras[palavra].dica;
 }
 geradorDePalavras();
+
+function palavrasNaTela() {
+  const dica = document.querySelector(".container-dica");
+  dica.innerHTML = "Dica: " + dicaSorteada;
+
+  const palavra = document.querySelector(".container-palavra-secreta");
+  palavra.innerHTML = "";
+
+  for (i = 0; i < palavraSorteada.length; i++) {
+    if (listaDinamica[i] == undefined) {
+      listaDinamica[i] = "&nbsp;";
+      palavra.innerHTML =
+        palavra.innerHTML +
+        "<div class='container-letra'>" +
+        listaDinamica[i] +
+        "</div>";
+    } else {
+      palavra.innerHTML =
+        palavra.innerHTML +
+        "<div class='container-letra'>" +
+        listaDinamica[i] +
+        "</div>";
+    }
+  }
+}
+palavrasNaTela();
+
+function teclaClicada(letra) {
+  if (contadorChances > 0) {
+    mudarStilo("tecla" + letra);
+    verificaLetra(letra);
+    palavrasNaTela();
+  }
+}
+
+function mudarStilo(tecla) {
+  document.getElementById(tecla).style.background = "#0a3871";
+  document.getElementById(tecla).style.color = "#ffffff";
+  document.getElementById(tecla).style.border = "1px solid black";
+}
+
+function verificaLetra(letra) {
+  const posicao = palavraSorteada.indexOf(letra);
+  if (posicao < 0) {
+    contadorChances--;
+  } else {
+    for (i = 0; i < palavraSorteada.length; i++) {
+      if (palavraSorteada[i] == letra) {
+        listaDinamica[i] = letra;
+      }
+    }
+  }
+  let vitoria = true;
+  for (i = 0; i < palavraSorteada.length; i++) {
+    if (palavraSorteada[i] != listaDinamica[i]) {
+      vitoria = false;
+    }
+  }
+  if (vitoria) {
+    alert("ganhou");
+    contadorChances = 0;
+  }
+}
